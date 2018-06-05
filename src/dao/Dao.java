8437acccf8058169a7106b941f028406f80ad2ca
muumiso <38120187+muumiso@users.noreply.github.com>
2018-06-05 15:46:34 +0900

    Dao作成 スキーマ名code

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -1,6 +1,48 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Dao
 {
+    protected Connection con;
 
+     public Dao(Connection con) {
+        super();
+        this.con = con;
+    }
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException
+     {
+        //接続文字列の構築
+        /* ユーザ名 */
+        String user = "root";
+        /* パスワード */
+        String pass = "root";
+
+        /* サーバ名 */
+        String servername = "localhost:3306";
+        /* DB名 */
+        String dbname = "code";
+
+        // ドライバーのロード
+        Class.forName("com.mysql.jdbc.Driver");
+
+        //com.mysql.jdbc.Driver d= new com.mysql.jdbc.Driver();
+        //com.mysql.jdbc.JDBC4Connection c = new com.mysql.jdbc.JDBC4Connection();
+
+        //接続の実行とコネクションオブジェクトの取得
+        Connection c = DriverManager.getConnection(
+                    "jdbc:mysql://"
+                    + servername
+                    + "/"
+                    + dbname,
+                    user,
+                    pass);
+
+
+        //取得したコネクションの返却
+        return c;
+     }
 }
