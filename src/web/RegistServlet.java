33f117ceefeddf357e4d6447ecb1f1ee1d148b3a
takata-enju <enju.takata@central-soft.co.jp>
2018-06-05 09:39:27 +0900

    プッシュ確認

--- src/web/RegistServlet.java
+++ src/web/RegistServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/RegistServlet")
 public class RegistServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-
+//ああ
     /**
      * @see HttpServlet#HttpServlet()
      */
