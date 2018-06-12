29090116940fc800741efae62ec7877b621ca71d
mahiron <38120190+mahiron@users.noreply.github.com>
2018-06-12 15:47:38 +0900

    [update]CoordinateSaleClothServletをSaleClothDisplayServletに変更

--- src/web/SaleClothDisplayServlet.java
+++ src/web/SaleClothDisplayServlet.java
@@ -18,15 +18,15 @@ import service.ClothService;
  * Servlet implementation class RegistServlet
  */
 //持ち服を基に一覧表示
-@WebServlet("/CoordinateSaleClothServlet")
-public class CoordinateSaleClothServlet extends HttpServlet
+@WebServlet("/SaleClothDisplayServlet")
+public class SaleClothDisplayServlet extends HttpServlet
 {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CoordinateSaleClothServlet()
+    public SaleClothDisplayServlet()
     {
         super();
     }
