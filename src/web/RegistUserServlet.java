db96c361d3f651e88c60c1da0b1b31c70c92c185
t-san <yuya.kunugi@central-soft.co.jp>
2018-06-13 12:50:10 +0900

    [add]ユーザー登録用jsp、サーブレット追加

---
+++ src/web/RegistUserServlet.java
@@ -0,0 +1,44 @@
+package web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegistUserServlet
+ */
+
+//ユーザー登録用サーブレット
+@WebServlet("/RegistUserServlet")
+public class RegistUserServlet extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegistUserServlet()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    //登録
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        System.out.println( "登録サーブレット" );
+        RequestDispatcher disp = request.getRequestDispatcher( "/registuser.jsp" );
+        disp.forward( request, response );
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        doGet( request, response );
+    }
+
+}
