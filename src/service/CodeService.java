b2b726ec8b939d73bc67c1758498e75ca664df94
mahiron <38120190+mahiron@users.noreply.github.com>
2018-06-13 16:18:48 +0900

    [update]Coordinate*

--- src/service/CodeService.java
+++ src/service/CodeService.java
@@ -1,32 +1,57 @@
 package service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import bean.CoordinateBean;
+import dao.Dao;
+import dao.SaveCodeDao;
+import vo.SaveCodeVo;
+
 //コーデ(取り出し・登録)のService
 public class CodeService
 {
     //コーデの情報取得
     //作成者<野間>
-    /*public CoordinateDisplayBean coordinateDisplay()
+    public  List<CoordinateBean> coordinateDisplay()
     {
-        CoordinateDisplayBean bean;
+        CoordinateBean bean;
+       // List<CoordinateBean>  listbean;
         try (
-                Connection con = Dao.getConnection();)
+                Connection con = Dao.getConnection();
+            )
         {
-            SaveCodeDao scdao = new SaveCodeDao( con );
+            SaveCodeDao scdao = new SaveCodeDao(con);
+             Collection<SaveCodeVo> list = scdao.getCodeCloth();
 
-            //登録したコーデをDBから取得
-            Collection<SaveCodeVo> list = scdao.getCodeCloth();
-            bean = new CoordinateDisplayBean();
-            //上下で分けて入れるList
-            List<CoordinateDisplayBean> topClothList = new ArrayList<CoordinateDisplayBean>();
-            List<CoordinateDisplayBean> bottomClothList = new ArrayList<CoordinateDisplayBean>();
-        } catch (ClassNotFoundException e) {
-            // TODO 自動生成された catch ブロック
+            List<CoordinateBean> codelist = new ArrayList<CoordinateBean>();
+            for (SaveCodeVo scvo : list)
+            {
+                bean = new CoordinateBean();
+                bean.setTopColor(scvo.getTopscolor());
+                bean.setTopCategory(scvo.getTopscategory());
+                bean.setTopPattern(scvo.getTopspattern());
+                bean.setTopSize(scvo.getTopssize());
+                bean.setBottomColor(scvo.getTopscolor());
+                bean.setBottomCategory(scvo.getBottomscategory());
+                bean.setBottomPattern(scvo.getTopspattern());
+                bean.setBottomSize(scvo.getTopssize());
+
+                codelist.add(bean);
+            }
+            //listbean.setList( codelist );
+
+            return codelist;
+
+        } catch (ClassNotFoundException | SQLException e)
+        {
             e.printStackTrace();
-        } catch (SQLException e) {
-            // TODO 自動生成された catch ブロック
-            e.printStackTrace();
+            throw new RuntimeException( e );
         }
-        return null;
-    }*/
+
+    }
 }
 
