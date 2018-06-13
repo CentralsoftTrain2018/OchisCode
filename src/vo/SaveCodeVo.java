1274c0d4dfa4c66e9dbda881cdc3e7e3bd9ee02d
muumiso <38120187+muumiso@users.noreply.github.com>
2018-06-13 14:44:22 +0900

    [fix]スペルミス修正

--- src/vo/SaveCodeVo.java
+++ src/vo/SaveCodeVo.java
@@ -12,13 +12,13 @@ public class SaveCodeVo
     protected int codenum;
     protected String userid;
     private ColorEnum topscolor;
-    private ColorEnum botomsscolor;
+    private ColorEnum bottomscolor;
     private CategoryEnum topscategory;
-    private CategoryEnum botomscategory;
-    private PatternEnum topspttern;
-    private PatternEnum botomspttern;
+    private CategoryEnum bottomscategory;
+    private PatternEnum topspattern;
+    private PatternEnum bottomspattern;
     private SizeEnum topssize;
-    private SizeEnum botomssize;
+    private SizeEnum bottomssize;
 
     public SaveCodeVo() {
 
@@ -27,24 +27,24 @@ public class SaveCodeVo
     public SaveCodeVo(int codenum,
             String userid,
             ColorEnum topscolor,
-            ColorEnum botomsscolor,
+            ColorEnum bottomscolor,
             CategoryEnum topscategory,
-            CategoryEnum botomscategory,
-            PatternEnum topspttern,
-            PatternEnum botomspttern,
+            CategoryEnum bottomscategory,
+            PatternEnum topspattern,
+            PatternEnum bottomspattern,
             SizeEnum topssize,
-            SizeEnum botomssize)
+            SizeEnum bottomssize)
     {
         this.codenum = codenum;
         this.userid = userid;
         this.topscolor = topscolor;
-        this.botomsscolor = botomsscolor;
+        this.bottomscolor = bottomscolor;
         this.topscategory = topscategory;
-        this.botomscategory = botomscategory;
-        this.topspttern = topspttern;
-        this.botomspttern = botomspttern;
+        this.bottomscategory = bottomscategory;
+        this.topspattern = topspattern;
+        this.bottomspattern = bottomspattern;
         this.topssize = topssize;
-        this.botomssize = botomssize;
+        this.bottomssize = bottomssize;
     }
 
     public int getCodenum() {
@@ -71,28 +71,12 @@ public class SaveCodeVo
         this.topscolor = topscolor;
     }
 
-    public ColorEnum getBotomsscolor() {
-        return botomsscolor;
+    public ColorEnum getBottomscolor() {
+        return bottomscolor;
     }
 
-    public void setBotomsscolor(ColorEnum botomsscolor) {
-        this.botomsscolor = botomsscolor;
-    }
-
-    public PatternEnum getTopspttern() {
-        return topspttern;
-    }
-
-    public void setTopspttern(PatternEnum topspttern) {
-        this.topspttern = topspttern;
-    }
-
-    public PatternEnum getBotomspttern() {
-        return botomspttern;
-    }
-
-    public void setBotomspttern(PatternEnum botomspttern) {
-        this.botomspttern = botomspttern;
+    public void setBottomscolor(ColorEnum bottomscolor) {
+        this.bottomscolor = bottomscolor;
     }
 
     public CategoryEnum getTopscategory() {
@@ -103,12 +87,28 @@ public class SaveCodeVo
         this.topscategory = topscategory;
     }
 
-    public CategoryEnum getBotomscategory() {
-        return botomscategory;
+    public CategoryEnum getBottomscategory() {
+        return bottomscategory;
     }
 
-    public void setBotomscategory(CategoryEnum botomscategory) {
-        this.botomscategory = botomscategory;
+    public void setBottomscategory(CategoryEnum bottomscategory) {
+        this.bottomscategory = bottomscategory;
+    }
+
+    public PatternEnum getTopspattern() {
+        return topspattern;
+    }
+
+    public void setTopspattern(PatternEnum topspattern) {
+        this.topspattern = topspattern;
+    }
+
+    public PatternEnum getBottomspattern() {
+        return bottomspattern;
+    }
+
+    public void setBottomspattern(PatternEnum bottomspattern) {
+        this.bottomspattern = bottomspattern;
     }
 
     public SizeEnum getTopssize() {
@@ -119,12 +119,13 @@ public class SaveCodeVo
         this.topssize = topssize;
     }
 
-    public SizeEnum getBotomssize() {
-        return botomssize;
+    public SizeEnum getBottomssize() {
+        return bottomssize;
     }
 
-    public void setBotomssize(SizeEnum botomssize) {
-        this.botomssize = botomssize;
+    public void setBottomssize(SizeEnum bottomssize) {
+        this.bottomssize = bottomssize;
     }
+
 
 }
