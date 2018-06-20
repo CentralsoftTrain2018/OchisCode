d874795e5e7f92b25766bfdd576c1115a9ca6a7e
takata-enju <enju.takata@central-soft.co.jp>
2018-06-20 14:35:37 +0900

    [update]手足付き画像追加とCategoryEnum列挙子追加

--- src/domain/CategoryEnum.java
+++ src/domain/CategoryEnum.java
@@ -2,7 +2,10 @@ package domain;
 
 public enum CategoryEnum
 {
-    Tシャツ(JougeEnum.上), 半ズボン(JougeEnum.下);
+    Tシャツ(JougeEnum.上), 半ズボン(JougeEnum.下),
+    セーター(JougeEnum.上),長ズボン(JougeEnum.下),
+    ロングシャツ(JougeEnum.上),ロングスカート(JougeEnum.下),
+    ミニスカート(JougeEnum.下);
     private JougeEnum jouge;
 
     private CategoryEnum(JougeEnum jouge)
